d2d7f9aeffd94e156ba4a0bea5af7a200b6eb1ad
dibya61 <dibyajyotihore.djh16@gmail.com>
2023-02-27 04:21:30 +0600

    new code

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 
+const users=["Dibya","Kammya","Salman","Farhan"];
+
 const app=express();
-app.get("/",(req,res)=>{res.send("Thank you for calling me")});
+app.get("/",(req,res)=>{
+    const fruit ={
+        product:"mango",
+        price:200
+    }
+    res.send(fruit);
+});
+app.get("/fruits/banana",(req,res)=>{
+   
+    res.send({fruit:"banana",quantity:3});
+});
+app.get("/users/:id",(req,res)=>{
+
+    const id=req.params.id;
+    const name=users[id];
+    res.send({id,name});
+});
+
 
 app.listen(3000,()=>console.log("Listening to port 3000"));
